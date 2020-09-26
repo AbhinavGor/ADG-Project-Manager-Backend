835018fc850115c7e40e3198ef2463937a6749cd
AbhinavGor <abhinav20016@gmail.com>
2020-09-26 19:58:14 +0530

    Added index.js and User model

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json())
+app.use(cors())
+
+//DB Config
+const db = "mongodb+srv://Abhinav123:Abhinav123@project-manager.ckyoe.mongodb.net/Project-Manager?retryWrites=true&w=majority"
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log("MONGDB Connected!!")
+}).catch((e) => {console.log("Unable to connect to MongoDB :'(.")})
+
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use(session({
+    secret: 'THISISASESSIONSECRET',
+    saveUninitialized: true,
+    resave: true,
+    cookie: { maxAge: 6000}
+}))
+
+//PassportJS setup
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+app.listen(PORT, () => {
+    console.log(`Server is walking on port: ${PORT}.`)
+})
